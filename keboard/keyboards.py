b796b5d1629127e23ba31e88a563d38789aae1e9
Cybersport <Cybersport@26kadr.internal>
2024-05-21 13:35:33 +0300

    5

--- keboard/keyboards.py
+++ keboard/keyboards.py
@@ -2,14 +2,30 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def get_keyboard_1():
     keyboard = ReplyKeyboardMarkup(resize_keyboard= True)
-    button_1 = KeyboardButton ('Отправь фото кота')
-    button_2 = KeyboardButton ('Перейти на следующую клавиатуру')
-    keyboard.add(button_1, button_2)
+    button_1 = KeyboardButton('Отправь фото кота')
+    button_2 = KeyboardButton('Перейти на вторую клавиатуру')
+    button_3 = KeyboardButton('Перейти на третью клавиатуру')
+    button_4 = KeyboardButton('Перейти на четвёртую клавиатуру')
+    keyboard.add(button_1, button_2, button_3, button_4)
     return keyboard
 
 def get_keyboard_2():
     keyboard_2 = ReplyKeyboardMarkup(resize_keyboard= True)
-    button_3 = KeyboardButton ('Отправь фото собаки')
-    button_4 = KeyboardButton ('Вернуться на 1 клавиатуру')
-    keyboard_2.add(button_3, button_4)
+    button_5 = KeyboardButton ('Отправь фото собаки')
+    button_6 = KeyboardButton ('Вернуться на 1 клавиатуру')
+    keyboard_2.add(button_5, button_6)
     return keyboard_2
+
+def get_keyboard_3():
+    keyboard_3 = ReplyKeyboardMarkup(resize_keyboard= True)
+    button_7 = KeyboardButton ('Отправь фото Максвелла')
+    button_8 = KeyboardButton ('Вернуться на 1 клавиатуру')
+    keyboard_3.add(button_7, button_8)
+    return keyboard_3
+
+def get_keyboard_4():
+    keyboard_4 = ReplyKeyboardMarkup(resize_keyboard= True)
+    button_9 = KeyboardButton ('Отправь фото Шлёппы')
+    button_10 = KeyboardButton ('Вернуться на 1 клавиатуру')
+    keyboard_4.add(button_9, button_10)
+    return keyboard_4
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher, types, executor
 from config import TELEGRAM_TOKEN
-from keboard.keyboards import get_keyboard_1, get_keyboard_2
+from keboard.keyboards import get_keyboard_1, get_keyboard_2, get_keyboard_3, get_keyboard_4
 
 bot = Bot(token= TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
@@ -25,17 +25,41 @@ async def start(message: types.Message):
 async def button_1_click(message: types.Message):
     await bot.send_photo(message.chat.id, photo= 'https://www.purina.co.uk/sites/default/files/styles/ttt_image_510/public/2020-12/Understanding%20Your%20Cat%27s%20Body%20LanguageHERO.jpg?itok=soc9Ieol', caption= 'Вот тебе кот!' )
 
-@dp.message_handler(lambda message: message.text == 'Перейти на следующую клавиатуру')
+@dp.message_handler(lambda message: message.text == 'Перейти на вторую клавиатуру')
 async def button_2_click(message: types.Message):
     await message.answer('Тут можно запросить фото собаки', reply_markup= get_keyboard_2())
 
+@dp.message_handler(lambda message: message.text == 'Перейти на третью клавиатуру')
+async def button_2_click(message: types.Message):
+    await message.answer('Тут можно запросить фото Максвелла', reply_markup=get_keyboard_3())
+
+@dp.message_handler(lambda message: message.text == 'Перейти на четвёртую клавиатуру')
+async def button_2_click(message: types.Message):
+    await message.answer('Тут можно запросить фото Шлёппы', reply_markup=get_keyboard_4())
+
 @dp.message_handler(lambda message: message.text == 'Отправь фото собаки')
 async def button_3_click(message: types.Message):
-    await bot.send_photo(message.chat.id, photo= 'https://www.purina.co.nz/sites/default/files/2020-12/Dog_1098119012_Teaser.jpg', caption= 'Вот тебе собака!' )
+    await bot.send_photo(message.chat.id, photo= 'https://www.purina.co.nz/sites/default/files/2020-12/Dog_1098119012_Teaser.jpg', caption= 'Вот тебе собака!')
 
 @dp.message_handler(lambda message: message.text == 'Вернуться на 1 клавиатуру')
 async def button_2_click(message: types.Message):
     await message.answer('Тут можно запросить фото кота', reply_markup= get_keyboard_1())
+
+@dp.message_handler(lambda message: message.text == 'Отправь фото Максвелла')
+async def button_1_click(message: types.Message):
+    await bot.send_photo(message.chat.id, photo= 'https://static.wikia.nocookie.net/silly-cat/images/d/d5/Maxwell.png/revision/latest?cb=20231001194454', caption= 'Вот тебе Максвелл!')
+
+@dp.message_handler(lambda message: message.text == 'Перейти на 1 клавиатуру')
+async def button_2_click(message: types.Message):
+    await message.answer('Тут можно запросить фото Максвелла', reply_markup= get_keyboard_1())
+
+@dp.message_handler(lambda message: message.text == 'Отправь фото Шлёппы')
+async def button_1_click(message: types.Message):
+    await bot.send_photo(message.chat.id, photo= 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Big_Floppa_and_Justin_2_%28cropped%29.jpg/528px-Big_Floppa_and_Justin_2_%28cropped%29.jpg', caption= 'Вот тебе Шлёппа!')
+
+@dp.message_handler(lambda message: message.text == 'Перейти на 1 клавиатуру')
+async def button_2_click(message: types.Message):
+    await message.answer('Тут можно запросить фото Шлёппы', reply_markup= get_keyboard_1())
 
 @dp.message_handler(commands= 'help')
 async def help(message: types.Message):
@@ -51,7 +75,6 @@ async def echo(message: types.Message):
 
 async def on_startup(dispatcher):
     await set_commands(dispatcher.bot)
-
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates= True, on_startup= on_startup)
